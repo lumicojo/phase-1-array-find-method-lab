c253904e2da24fe4555bb55d7bee5e65268f9945
+ <+>
2022-10-04 07:46:25 -0400

    commit tree f24e0b3b3305bf07e33fb4ba83d0e0d65f1c6cd8

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record) {
+}
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+];
+function isSuperBowl(won) {
+    return won.name === "Denver Broncos";
+}
+console.log(record.find(isW));
